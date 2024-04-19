5bf211aea587e12654cac7067a9125690e1947c7
MuriloRieth <167137795+MuriloRieth@users.noreply.github.com>
2024-04-18 22:29:33 -0300

    Create atividade1.java

---
+++ atividade1.java
@@ -0,0 +1,25 @@
+class HelloWorld {
+    public static void main(String[] args) {
+       String Nome = "Murilo Rieth Rovea";
+       String Sobrenome = "Rovea";
+       int Idade = 21;
+       int tempo = 15;
+       char Sexo = 'M';
+       boolean Smoke = false;
+       boolean Drink = true;
+       String Time = "Inter";
+       String Game = "Fifa";
+       boolean set = false ;
+
+
+
+
+
+        System.out.println("SUA IDADE EH :"+Idade);
+
+
+
+    }
+
+
+}
