1cd5181d20b6f0e4a507a951458ff0ed93f9f4f9
MuriloRieth <167137795+MuriloRieth@users.noreply.github.com>
2024-04-18 22:32:04 -0300

    Create atividade3.java

---
+++ atividade3.java
@@ -0,0 +1,17 @@
+class HelloWorld {
+    public static void main(String[] args) {
+
+    String NomeDoAluno = "Murilo Rieth Rovea";
+    int IdadeDoAluno = 21;
+    int NotaDaProva1 = 8;
+    int NotaDaProva2 = 8;
+    int MediaFinal =8;
+
+        System.out.println(MediaFinal);
+
+
+
+    }
+
+
+}
