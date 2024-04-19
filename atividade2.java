7c0be6779f9d503c940659c57886d25fa0c2c7f3
MuriloRieth <167137795+MuriloRieth@users.noreply.github.com>
2024-04-18 22:30:59 -0300

    Create atividade2.java

---
+++ atividade2.java
@@ -0,0 +1,15 @@
+class HelloWorld {
+    public static void main(String[] args) {
+    String NomeProduto1 = "Tenis nike sb dunk low pro";
+    double PrecoProduto =520;
+    String MarcaDoProduto = "Nike";
+    int QuantidadeProduto = 1;
+    String Descricao_Produto = "Tenis nike numero 40";
+    String NomeProduto = "Tenis nike";
+    int Numero_Produto123 = 40; 
+    int PrecoDoProduto = 520;
+    boolean EstoqueDisponivel = false;  
+    int TotalDeProdutos = 10;
+
+
+        System.out.println(Descricao_Produto);
